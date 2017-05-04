12f6b4b64e192b6c30ce6eb347c3cf9f33435258
Tanner Hoelzel <thoelze1@binghamton.edu>
2017-05-04 18:49:50 -0400

    successfully writes adj matrices

--- tspToTxt.py
+++ tspToTxt.py
@@ -1,16 +1,37 @@
 # copied directly from http://esa.github.io/pygmo/tutorials/create_tsp.html
 
-from PyGMO.util import tsp as tsputil
-from PyGMO.problem import tsp
+# from PyGMO.util import tsp as tsputil
+import os
+import PyGMO.util
+# from PyGMO.problem import tsp
 
 # importing the XML file
-weights = tsputil.read_tsplib('burma14.xml')
+# weights = tsputil.read_tsplib('burma14.xml')
+
+for xml in os.listdir("tsplib-xml/"):
+  txt = "tsplib-mat/" + os.path.splitext(xml)[0] + ".txt"
+  xml = "tsplib-xml/" + xml
+  # print(xml)
+  # print(txt)
+  adjMatrix = open(txt,"w")
+  weights = PyGMO.util._tsp.read_tsplib(xml)
+  high = 0
+  for line in weights:
+    for value in line:
+      if value > high:
+        high = value
+  width = len(str(high)) + 1
+  for line in weights:
+    for value in line:
+      adjMatrix.write(str(value).rjust(width));
+    adjMatrix.write("\n");
 
 # printing the weights matrix
-tsputil.print_matrix(weights)
+# tsputil.print_matrix(weights)
+PyGMO.util._tsp._print_matrix(weights)
 
 # creating a tsp problem from the imported weights matrix
-tsp_instance = tsp(weights)
+# tsp_instance = tsp(weights)
 
 # printing the tsp problem details to console
-print tsp_instance
+# print tsp_instance
